ad335cbdb9ee0bd278477077bf8e3c744653c169
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2019-07-01 23:13:38 -0400

    update

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,15 @@
-
 $(document).on('click', 'a ', function(){
     $(this).addClass('active').siblings().removeClass('active')
 })
 
+var menuButton = document.querySelector(".menu-button");
 
+ menuButton.addEventListener("click", function(event) {
+   event.preventDefault();
+   var parent = document.querySelector(".menu");
+   if (parent.classList.contains("open")) {
+     parent.classList.remove("open");
+   } else {
+     parent.classList.add("open");
+   }
+});
