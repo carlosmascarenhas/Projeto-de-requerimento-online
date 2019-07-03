5eba33a71b0f906567ac62465773c1d6dc3d4c04
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2019-07-03 10:19:06 -0400

    Arrumando menu

--- js/principal.js
+++ js/principal.js
@@ -6,12 +6,8 @@ var menuButton = document.querySelector(".menu-button");
 
  menuButton.addEventListener("click", function(event) {
    event.preventDefault();
-   var parent = document.querySelector(".menu");
-   if (parent.classList.contains("open")) {
-     parent.classList.remove("open");
-   } else {
-     parent.classList.add("open");
-   }
+   document.querySelector('.menu-dropdown').classList.toggle('hidden');
+   document.querySelector('.menu-button').classList.toggle('text-black');
 });
 
 
