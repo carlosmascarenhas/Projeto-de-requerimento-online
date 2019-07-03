3a0edd20d408e269fcc52bbcd738a2baa6284be5
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2019-07-03 15:19:45 -0400

    Resolve clicks do menu

--- js/principal.js
+++ js/principal.js
@@ -4,19 +4,28 @@ $(document).on('click', '.icone', function(){
 
 document.querySelector(".menu-button").addEventListener("click", function(event) {
    event.preventDefault();
+   event.stopPropagation();
    document.querySelector('.menu-dropdown').classList.remove('hidden');
    document.querySelector('.menu-button').classList.add('text-black');
 });
 
 document.getElementsByTagName('body')[0].addEventListener('click', function(event){
-  console.log(event.target.classList)
   if(!event.target.classList.contains('.menu-button')){
     document.querySelector('.menu-dropdown').classList.add('hidden');
     document.querySelector('.menu-button').classList.remove('text-black');
   }
 });
 
+function hidden_containers(){
+  document.querySelectorAll('.container').forEach(function(element){
+    element.classList.add('displayNone');
+  });
+}
 
-
-
-
+document.querySelector('.icones').addEventListener('click', function(event){
+  if(event.target !== event.currentTarget){
+    let container_name = event.target.dataset.container;
+    hidden_containers();
+    document.querySelector(`.${container_name}`).classList.remove('displayNone');
+  }
+});
