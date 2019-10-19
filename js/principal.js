e3b71c55d80b85549eb4d6aaf3fde7c31415c91b
carlos-eduard0 <eduard.mascarenhas@hotmail.com>
2019-10-18 22:54:32 -0400

    update responsivo

--- js/principal.js
+++ js/principal.js
@@ -30,7 +30,7 @@ document.querySelector('.icones').addEventListener('click', function (event) {
   }
 });
 $(document).ready(function() {
-  $('.menu-des-mobile').slideAndSwipe();
+  $('.nav').slideAndSwipe();
 });
 // modal que receberá as informações do usuário
 $('.lista li').click(function () {
